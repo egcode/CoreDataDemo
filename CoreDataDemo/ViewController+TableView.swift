e633a193e00be9ca5a7358716419df01c0007fca
Eugene <eugene@arcules.com>
2022-04-27 18:15:17 -0700

    manage code

--- CoreDataDemo/ViewController+TableView.swift
+++ CoreDataDemo/ViewController+TableView.swift
@@ -21,78 +21,18 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let item = UIContextualAction(style: .destructive, title: "RemovePers") {  (contextualAction, view, boolValue) in
-         
-            // - Person to remove
-            let personToRemove = self.people![indexPath.row]
-            
-            // - Remove the person
-            self.context.delete(personToRemove)
-            
-            // - Save the data
-            do {
-                try self.context.save()
-            } catch {
-                print("Unable save after delet a person: \(error)")
-            }
-            
-            // - Refetch the data
-            self.fetchPeople()
+            self.removePerson(indexPath: indexPath)
         }
         
         item.image = UIImage(named: "deleteIcon")
-
         let swipeActions = UISwipeActionsConfiguration(actions: [item])
-    
         return swipeActions
-
     }
-    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedPerson = self.people![indexPath.row]
-        
-        
-        let alertController = UIAlertController(title: "Title", message: "", preferredStyle: .alert)
-        alertController.addTextField { (textField : UITextField!) -> Void in
-            textField.text = selectedPerson.name
-        }
-        let saveAction = UIAlertAction(title: "Update", style: .default, handler: { alert -> Void in
-            if let textField = alertController.textFields?[0] {
-                if let txt = textField.text {
-                    print("Person Updated :: \(txt)")
-                    
-                    // - Update CoreData Person name
-                    selectedPerson.name = txt
-                    
-                    // - Save the Data
-                    do {
-                        try self.context.save()
-                    } catch {
-                        print("Unable To save person \(error)")
-                    }
-                    
-                    // - Re-Fetch the data
-                    self.fetchPeople()
-                    
-                    
-                } else {
-                    print("No text to add")
-                }
-            }
-        })
-        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: {
-            (action : UIAlertAction!) -> Void in })
-
-        alertController.addAction(cancelAction)
-        alertController.addAction(saveAction)
-        alertController.preferredAction = saveAction
-        self.present(alertController, animated: true, completion: nil)
-
-        
-        
-        
-        
+        self.editPerson(indexPath: indexPath)
     }
+
     
     // MARK: - Table view data source
     
@@ -103,55 +43,6 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = person.name
         return cell
     }
-
-    
-    /*
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -57,7 +57,7 @@ class ViewController: UIViewController {
     }
 
 
-    // MARK: - CoreData Add
+    // MARK: - CoreData Add, Remove, Edit
 
     @objc func addPerson() {
         let alertController = UIAlertController(title: "Title", message: "", preferredStyle: .alert)
@@ -100,7 +100,65 @@ class ViewController: UIViewController {
         alertController.addAction(saveAction)
         alertController.preferredAction = saveAction
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func removePerson(indexPath: IndexPath) {
+        
+        // - Person to remove
+        let personToRemove = self.people![indexPath.row]
 
+        // - Remove the person
+        self.context.delete(personToRemove)
+
+        // - Save the data
+        do {
+            try self.context.save()
+        } catch {
+            print("Unable save after delet a person: \(error)")
+        }
+
+        // - Refetch the data
+        self.fetchPeople()
+    }
+    
+    func editPerson(indexPath: IndexPath) {
+        let selectedPerson = self.people![indexPath.row]
+        
+        let alertController = UIAlertController(title: "Title", message: "", preferredStyle: .alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.text = selectedPerson.name
+        }
+        let saveAction = UIAlertAction(title: "Update", style: .default, handler: { alert -> Void in
+            if let textField = alertController.textFields?[0] {
+                if let txt = textField.text {
+                    print("Person Updated :: \(txt)")
+                    
+                    // - Update CoreData Person name
+                    selectedPerson.name = txt
+                    
+                    // - Save the Data
+                    do {
+                        try self.context.save()
+                    } catch {
+                        print("Unable To save person \(error)")
+                    }
+                    
+                    // - Re-Fetch the data
+                    self.fetchPeople()
+                    
+                    
+                } else {
+                    print("No text to add")
+                }
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: {
+            (action : UIAlertAction!) -> Void in })
+
+        alertController.addAction(cancelAction)
+        alertController.addAction(saveAction)
+        alertController.preferredAction = saveAction
+        self.present(alertController, animated: true, completion: nil)
     }
 }
 
